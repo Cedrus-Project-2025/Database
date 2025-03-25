aed1d2bb8588b9f28c81380137d949cdb1b8eddc
monroyminerodiego <diego.monroy.minero@outlook.com>
2025-03-24 21:31:23 -0600

    Hotfix para que funcione

--- Files/Scripts/python/scheduler/backup_scheduler.py
+++ Files/Scripts/python/scheduler/backup_scheduler.py
@@ -8,7 +8,7 @@ updt_file_path = os.path.join(
     "Temp",
     "update_file.txt"
 )
-RCLONE_BIN = os.path.abspath(location_path, "./installation/rclone")
+RCLONE_BIN = os.path.join(location_path, "./installation/rclone")
 
 
 
